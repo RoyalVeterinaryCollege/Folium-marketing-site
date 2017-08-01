eea594fdd3488651181529882beeacb248093791
Peter Nunn <prnunn@gmail.com>
2017-08-01 10:10:19 +0100

    Folium Marketing Site Initial Upload

---
+++ js/creative.js
@@ -0,0 +1,90 @@
+(function($) {
+    "use strict"; // Start of use strict
+
+    // Smooth scrolling using jQuery easing
+    $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: (target.offset().top - 48)
+                }, 1000, "easeInOutExpo");
+                return false;
+            }
+        }
+    });
+
+    // Activate scrollspy to add active class to navbar items on scroll
+    $('body').scrollspy({
+        target: '#mainNav',
+        offset: 48
+    });
+
+    // Closes responsive menu when a link is clicked
+    $('.navbar-collapse>ul>li>a').click(function() {
+        $('.navbar-collapse').collapse('hide');
+    });
+
+    // Collapse the navbar when page is scrolled
+    $(window).scroll(function() {
+        if ($("#mainNav").offset().top > 100) {
+            $("#mainNav").addClass("navbar-shrink");
+        } else {
+            $("#mainNav").removeClass("navbar-shrink");
+        }
+    });
+
+    // Scroll reveal calls
+    window.sr = ScrollReveal();
+    sr.reveal('.sr-image', {
+        duration: 1200,
+        scale: 0.3,
+        distance: '50px'
+    }, 200);
+    sr.reveal('#skills-screen', {
+        duration: 600,
+        scale: 0.3,
+        origin:'left',
+        distance: '500px',
+    
+    }, 200);
+    sr.reveal('#entries-screen', {
+        duration: 600,
+        scale: 0.3,
+        origin:'right',
+        distance: '500px'
+    }, 200);
+    sr.reveal('#placements-screen', {
+        duration: 600,
+        scale: 0.3,
+        origin:'left',
+        distance: '500px'
+    }, 200);
+    sr.reveal('.sr-button', {
+        duration: 600,
+        delay: 200
+    });
+    sr.reveal('.sr-contact', {
+        duration: 600,
+        scale: 0.3,
+        distance: '0px'
+    }, 300);
+
+    // Magnific popup calls
+    $('.popup-gallery').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        tLoading: 'Loading image #%curr%...',
+        mainClass: 'mfp-img-mobile',
+        gallery: {
+            enabled: true,
+            navigateByImgClick: true,
+            preload: [0, 1]
+        },
+        image: {
+            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
+        }
+    });
+
+})(jQuery); // End of use strict
